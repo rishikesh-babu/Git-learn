37dd40308d581ed3d7725312142fb358bbc1121d
Rishikesh Babu <rishikeshbabu2004@gmail.com>
2026-02-22 13:48:31 +0530

    change nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Component/Navbar'
 function App() {
     return (
         <>
-            <Navbar />
+            nav commit 
             layout commit  
             footer commit
         </>
